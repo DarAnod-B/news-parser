4b46b63b0d697e9713dae7c6428709d643e6f4d5
David Butaev <79657855+DarAnod-B@users.noreply.github.com>
2023-02-08 20:33:00 +0300

    add rewrite

--- dutchnews/pipelines.py
+++ dutchnews/pipelines.py
@@ -5,19 +5,44 @@
 
 
 # useful for handling different item types with a single interface
-import pandas as pd
-
-
 
 import pandas as pd
+import openai
+
+# Set up the OpenAI API key
+openai.api_key = "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"
+tags_ignored_rewrite = ['H2']
+
 
 class DutchnewsPipeline:
+
     def __init__(self):
         self.df = pd.DataFrame(columns=['tag', 'text'])
 
     def process_item(self, item, spider):
+        def clean_text(text):
+            text = text.strip()  # remove spaces from the beginning and end of the text
+            text = text.rstrip('\n')  # remove line breaks
+            return text
+
+        for count, text in enumerate(item['text']):
+            if item['tag'][count] not in tags_ignored_rewrite:
+                # The prompt to be passed to the API
+                prompt = "Rewrite the following sentence, except quotes, don't put quotes where there are no quotes,  don't create new quotes, use only existing ones: \n" + text
+
+                # Call the API and get the response
+                completions = openai.Completion.create(
+                    engine="text-davinci-002",
+                    prompt=prompt,
+                    max_tokens=1024,
+                    n=1,
+                    stop=None,
+                    temperature=0.5,
+                )
+                item['text'][count] = clean_text(completions.choices[0].text)
+
         self.df = self.df.append(pd.DataFrame(item), ignore_index=True)
         return item
 
     def close_spider(self, spider):
-        self.df.to_excel(f"{spider.name}.xlsx", index=False)
+        self.df.to_excel(f"{spider.name}_rec.xlsx", index=False)
--- dutchnews/spiders/dutch_news.py
+++ dutchnews/spiders/dutch_news.py
@@ -1,4 +1,15 @@
 import scrapy
+from enum import Enum
+
+
+class Xpath(Enum):
+    news_categories = r'//*[@id="body"]/section/div/div[1]/div/h3/a/@href'
+    news_article = r'//*[@id="body"]/section/div/div[1]/div[3]/ul/li/a/@href' 
+    text_header = r'//header/h1/text()'
+    element_p = r'//div[contains(@class, "entry-content ")]/p'
+    elements_containing_text = r'./text() | a/text() | ./strong/text()'
+    text_inside_paragraphs = r'./text() | a/text()'
+    
 
 
 class DutchNewsSpider(scrapy.Spider):
@@ -11,36 +22,48 @@ class DutchNewsSpider(scrapy.Spider):
 
     def parse(self, response):
         # follow the list of links to news categories
-        links = response.xpath(
-            '//*[@id="body"]/section/div/div[1]/div/h3/a/@href').getall()
+        links = response.xpath(Xpath.news_categories.value).getall()[:1]
         for link in links:
             yield response.follow(link, self.parse_news)
+
+    # def parse(self, response):
+    #     # follow the list of links to news categories
+    #     links = response.xpath(Xpath.news_categories.value).getall()
+    #     for link in links:
+    #         yield response.follow(link, self.parse_news)
 
     def parse_news(self, response):
         # extract the text from the header of each news article
         links = response.xpath(
-            '//*[@id="body"]/section/div/div[1]/div[3]/ul/li/a/@href').getall()
+            Xpath.news_article.value).getall()[:1]
         for link in links:
             yield response.follow(link, self.parse_item)
+
+
+    # def parse_news(self, response):
+    #     # extract the text from the header of each news article
+    #     links = response.xpath(
+    #         Xpath.news_article.value).getall()
+    #     for link in links:
+    #         yield response.follow(link, self.parse_item)
 
     def parse_item(self, response):
         item = {}
         tags = []
         texts = []
 
-        # extract the text from the header
-        header_text = response.xpath('//header/h1/text()').get()
+        # extract the text and tag from the header
+        header_text = response.xpath(Xpath.text_header.value).get()
         tags.append('H1')
         texts.append(header_text)
 
-        # extract the text from the content
-        content_text = response.xpath(
-            '//div[contains(@class, "entry-content ")]/p')  
+        # extract the text and tag from the content
+        content_text = response.xpath(Xpath.element_p.value)  
 
         for el in content_text:
-            text = el.xpath('./text() | a/text() | ./strong/text()').getall()
+            text = el.xpath(Xpath.elements_containing_text.value).getall()
             full_text = ''.join(text)
-            tag = 'p' if el.xpath('./text() | a/text()').getall() else 'H2'
+            tag = 'p' if el.xpath(Xpath.text_inside_paragraphs.value).getall() else 'H2'
             tags.append(tag)
             texts.append(full_text)
 
