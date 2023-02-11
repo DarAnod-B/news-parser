04593623c863b930b69b823736270f4e807f7634
David Butaev <79657855+DarAnod-B@users.noreply.github.com>
2023-02-11 14:41:47 +0300

    Update pipelines.py

--- dutchnews/pipelines.py
+++ dutchnews/pipelines.py
@@ -4,14 +4,14 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-
 import pandas as pd
 import openai
+import os
+import time
 
-# Set up the OpenAI API key
-openai.api_key = "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"
-tags_ignored_rewrite = ['H2']
+openai.api_key = os.environ.get('gpt_api_key')
+
+tags_ignored_rewrite = ['H1', 'H2']
 
 
 class DutchnewsPipeline:
@@ -21,28 +21,70 @@ class DutchnewsPipeline:
 
     def process_item(self, item, spider):
         def clean_text(text):
-            text = text.strip()  # remove spaces from the beginning and end of the text
-            text = text.rstrip('\n')  # remove line breaks
+            text = text.strip()
+            text = text.replace("\n", "")
+            if text.startswith('"') and text.endswith('"'):
+                if '"' not in text[1:-1]:
+                    text = text[1:-1]
             return text
 
-        for count, text in enumerate(item['text']):
-            if item['tag'][count] not in tags_ignored_rewrite:
-                # The prompt to be passed to the API
-                prompt = "Rewrite the following sentence, except quotes, don't put quotes where there are no quotes,  don't create new quotes, use only existing ones: \n" + text
+        def combining_text_paragraphs(item):
+            list_text = []
+            list_tag = []
+            for text, tag in zip(item['text'], item['tag']):
+                if tag in ['H1', 'H2']:
+                    list_tag.append(tag)
+                    list_text.append(text)
+                    list_tag.append([])
+                    list_text.append([])
+                else:
+                    list_tag[-1].append(tag)
+                    list_text[-1].append(text)
 
-                # Call the API and get the response
-                completions = openai.Completion.create(
-                    engine="text-davinci-002",
-                    prompt=prompt,
-                    max_tokens=1024,
-                    n=1,
-                    stop=None,
-                    temperature=0.5,
-                )
-                item['text'][count] = clean_text(completions.choices[0].text)
+            item['text'] = [''.join(text) for text in list_text]
+            item['tag'] = [tag[0] if type(
+                tag) == list else tag for tag in list_tag]
+            return item
 
-        self.df = self.df.append(pd.DataFrame(item), ignore_index=True)
-        return item
+        def text_rewriting(item):
+            count = 0
+            for text, tag in zip(item['text'], item['tag']):
+                if tag not in tags_ignored_rewrite:
+                    status = 'Not completed'
+                    rate_limit_retry_count = 10
+                    prompt = "Rewrite the following sentence, except quotes, add more uniqueness, the text should deceive the anti-plagiarism service: \n" + text
+
+                    while status != 'OK':
+                        try:
+                            completions = openai.Completion.create(
+                                engine="text-davinci-003",
+                                presence_penalty=0.8,
+                                prompt=prompt,
+                                max_tokens=1024,
+                                n=1,
+                                stop=None,
+                                temperature=0.7,
+                            )
+                            status = 'OK'
+                        except openai.error.RateLimitError:
+                            status == 'ERROR'
+                            rate_limit_retry_count -= 1
+                            if rate_limit_retry_count == 0:
+                                break
+                            else:
+                                time.sleep(60)
+
+                    item['text'][count] = clean_text(
+                        completions.choices[0].text)
+                count += 1
+                time.sleep(20)
+            return item
+
+        item_rewriting = text_rewriting(combining_text_paragraphs(item))
+
+        self.df = self.df.append(pd.DataFrame(
+            item_rewriting), ignore_index=True)
+        return item_rewriting
 
     def close_spider(self, spider):
         self.df.to_excel(f"{spider.name}_rec.xlsx", index=False)
